79e1334581a61c1b15c8667b22a40c2c244c16e7
Born-Bohrer <chem1000leaves@gmail.com>
2025-08-11 18:20:23 +0900

    fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,18 +1,14 @@
-import type { MetadataRoute } from "next";
+import type { MetadataRoute } from 'next';
 
-const urls: readonly string[] = [
-    '/',
-    '/conlang-gacha',
-    '/comma',
-];
+const urls: readonly string[] = ['/', '/conlang-gacha', '/comma'];
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    
-
-    return urls.map((url) => {
-        return {
-            url,
-            lastModified: new Date(),
-        }
-    });
+  return urls.map((url) => {
+    return {
+      url,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: url === '/' ? 1 : 0.8,
+    };
+  });
 }
