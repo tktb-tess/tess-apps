c57e810f1cc011ca41d4c3849f8bcc4843b1238f
Born-Bohrer <chem1000leaves@gmail.com>
2025-08-14 23:59:27 +0900

    タイトルアニメーション

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,16 @@ export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang='ja'>
       <body>
+        <nav className='flex border-b border-white/60 justify-center'>
+          <a
+            href='https://tktb-tess.dev'
+            target='_blank'
+            rel='noopener'
+            className='text-white h-12 flex items-center hover:bg-white hover:text-black no-underline transition-colors px-1'
+          >
+            悠久肆方体へ
+          </a>
+        </nav>
         <div className='px-2 w-full min-h-screen max-w-384 mx-auto flex flex-col gap-2'>
           {children}
         </div>
