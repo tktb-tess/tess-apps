8f3da728098dbece47a12acb4de91b8d9657fd89
tktb-tess <chemusiclove6174@gmail.com>
2026-03-22 01:16:51 +0900

    font

--- src/app/comma/(searching)/search/CommaResult.tsx
+++ src/app/comma/(searching)/search/CommaResult.tsx
@@ -1,7 +1,6 @@
 import type { CommaKind, Match } from '@/lib/mod/decl';
 import CommaResultView from './CommaResultView';
 import { fetchComma } from './funcs';
-import { redirect } from 'next/navigation';
 
 interface Props {
   params: {
@@ -15,10 +14,6 @@ interface Props {
 const CommaResult = async ({ params }: Props) => {
   const { query, query2, match, kind } = params;
   const commaData = await fetchComma(query, query2, kind, match);
-
-  if (commaData.length === 0) {
-    redirect('/comma');
-  }
 
   return <CommaResultView commaData={commaData} />;
 };
